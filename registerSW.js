3ef54b16cf4dd9ab330c2059996943288dc3f60a
Javier Arocha <arocha.grafico@gmail.com>
2026-06-12 16:32:24 +0200

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pelican-expiry-prototype/sw.js', { scope: '/pelican-expiry-prototype/' })})}
